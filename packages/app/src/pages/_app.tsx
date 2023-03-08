e34f91dd52cab733da8250d61a66220e64220ec0
Amon Sawamura <amotarao@gmail.com>
2023-03-08 11:42:43 +0900

    ヘッダー追加

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -2,5 +2,10 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <header className="grid h-[80px] place-items-center text-[20px] font-bold">View Transitions API</header>
+      <Component {...pageProps} />
+    </div>
+  );
 }
